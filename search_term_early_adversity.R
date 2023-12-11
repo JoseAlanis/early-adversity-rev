114fbaa246c7eb58faa739f44e02cca92171e635
José C. García Alanis <12409129+JoseAlanis@users.noreply.github.com>
2023-12-11 16:00:40 +0100

    Add files via upload

---
+++ search_term_early_adversity.R
@@ -0,0 +1,31 @@
+# 6
+
+AB = ( 	 
+  
+  ( 	 	
+    
+    ( "early" OR "postpart*" ) 	 	 	 	 	 
+    
+    NEAR/3
+    
+    ( "adversity"	OR "trauma" OR "violence" OR "maltreat*" OR	"harm" OR "neglect" OR "abuse" OR "stress*" ) 
+    
+  ) 
+  
+) 
+
+AND 	 	 
+
+AB = ( 	 	
+  
+  ( "child*" OR "adolesc*" ) 	 	 	
+  
+) 	 	
+
+AND 
+
+PY = ( (1990-2020) ) 	 	 	 	 
+
+AND 
+
+WC = ( "PSYCHOLOGY" or "PSYCHIATRY" OR "NEUROSCIENCES") 
